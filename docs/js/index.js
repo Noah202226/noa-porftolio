5eeeae4abbfd1dd4f4024c72dee342a84349eb70
noah <noaligpitan26@gmail.com>
2025-01-14 14:53:08 +0800

    add copy of static files to docs folder

---
+++ docs/js/index.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const message = document.getElementById("message");
+  message.textContent = "JavaScript is successfully linked!";
+});
