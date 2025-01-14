481f7da698a3af5892ef84ae59a80aeaae4620bf
noah <noaligpitan26@gmail.com>
2025-01-14 15:02:27 +0800

    add procfile

--- docs/js/index.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const message = document.getElementById("message");
-  message.textContent = "JavaScript is successfully linked!";
-});
